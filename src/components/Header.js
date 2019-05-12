6a29926e86fc4f70fc0da3a77a5054fe27a9327e
amalius-99 <amaliustapalov@gmail.com>
2019-05-12 19:48:57 +0200

    Stopwatch functionality added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Stats from "./Stats";
+import Stopwatch from "./Stopwatch";
 
 function Header(props) {
 	return (
 		<header>
 			<Stats players={props.players} />
 			<h1>{props.title}</h1>
+			<Stopwatch />
 		</header>
 	);
 }
@@ -17,5 +20,14 @@ function Header(props) {
 // 		<span>Players: 1</span>
 // 	</header>
 // );
+
+Header.propTypes = {
+	title: PropTypes.string,
+	players: PropTypes.arrayOf(PropTypes.object)
+};
+
+Header.defaultProps = {
+	title: "Scoreboard"
+};
 
 export default Header;
